b9215ea6cb4c8575d0c06f5256533a18263fced3
sonia1997 <30790990+sonia1997@users.noreply.github.com>
2019-09-22 01:43:18 +0530

    modified

--- demo-element.js
+++ demo-element.js
@@ -1,4 +1,4 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-list/iron-list.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import '@polymer/iron-icon/iron-icon.js';
