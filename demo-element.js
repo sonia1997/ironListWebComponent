a44d7b9da930886f64cfd45956aae949c3eeec4f
sonia1997 <30790990+sonia1997@users.noreply.github.com>
2019-09-22 13:46:32 +0530

    Modified

--- demo-element.js
+++ demo-element.js
@@ -1,65 +1,31 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer';
 import '@polymer/iron-list/iron-list.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import '@polymer/iron-icon/iron-icon.js';
-// import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icons/iron-icons.js';
 
-import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js?module'
+// import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js?module'
 // import '@polymer/iron-list/iron-list.js';
-import '../iron-list.js'
+import '../rating-list.js'
 
 class DemoElement extends PolymerElement {
   static get template() {
     return html`
     <style>
-    /* :host {
+    :host {
 font-family: sans-serif;
 --icon-toggle-color: lightgrey;
 --icon-toggle-outline-color: black;
 --icon-toggle-pressed-color: yellow;
-} */
+}
     </style>
 
 <center>
        <h3>Sample rating bar web component</h3>
-
-       <!-- <iron-ajax url="data.json" last-response="{{data}}" auto></iron-ajax> -->
-       <!-- <iron-list given="xyz" marks="900"></iron-list> -->
-       <!-- <iron-list toggle-icon="star"></iron-list> -->
-
-       <iron-list toggle-icon="star"> </iron-list>
-
-      <!-- <iron-list item-list="[[data]]" as="item">
-        <template>
-          <div>
-            Name: [[item.name]]
-          </div>
-        </template>
-      </iron-list> -->
-
-       <!-- <iron-list item-list="star"></iron-list> -->
-
-      <!-- <icon-toggle toggle-icon="star"></icon-toggle>
-      <icon-toggle toggle-icon="add"></icon-toggle>
-      <icon-toggle toggle-icon="settings"></icon-toggle> -->
+       <rating-list toggle-icon="star"> </rating-list>
 </center>
 
-      <!-- <h3>Data-bound icon-toggle</h3> -->
-
-      <!-- use a computed binding to generate the message -->
-      <!-- <div><span>[[_message(isFav)]]</span></div> -->
-
-      <!-- curly brackets ({{}}} allow two-way binding -->
-      <!-- <icon-toggle toggle-icon="favorite" pressed="{{isFav}}"></icon-toggle> -->
     `;
   }
-  // _message(fav) {
-  //   if (fav) {
-  //     return 'You really like me!';
-  //   }
-  //   else {
-  //     return 'Do you like me?';
-  //   }
-  // }
 }
 customElements.define('demo-element', DemoElement);
---
+++ rating-list.js
@@ -0,0 +1,86 @@
+import { PolymerElement, html } from '@polymer/polymer';
+import '@polymer/iron-list/iron-list.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
+import '@polymer/iron-icon/iron-icon.js';
+// import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js?module'
+
+class RatingList extends PolymerElement {
+
+  static get properties() {
+    return {
+
+      toggleIcon: {
+             type: String
+           },
+
+           pressed: {
+                 type: Boolean,
+                 value: false,
+                 notify: true,
+                 reflectToAttribute: true
+               },
+
+           variousIcons: {
+             type: Array,
+             value() {
+               return [
+                 {symbol: 'star'},
+                 {symbol: 'star'},
+                 {symbol: 'star'},
+                 {symbol: 'star'},
+                 {symbol: 'star'}
+               ];
+             }
+           },
+
+      employees: {
+        type: Array,
+        value() {
+          return [
+            {given: 'Sonia', marks: '50'},
+            {given: 'Priya', marks: '50'},
+          ];
+        }
+      }
+
+    };
+  }
+
+  static get template() {
+    return html`
+
+    <style>
+              :host {
+          display: inline-block;
+          }
+          iron-icon {
+          fill: var(--icon-toggle-color, rgba(0,0,0,0));
+          stroke: var(--icon-toggle-outline-color, currentcolor);
+          }
+          :host([pressed]) iron-icon {
+          fill: var(--icon-toggle-pressed-color, currentcolor);
+          }
+</style>
+      <div> My list: </div>
+      <!-- <iron-icon icon="[[toggleIcon]]"></iron-icon> -->
+
+      <template is="dom-repeat" items="{{variousIcons}}">
+      <iron-icon icon="[[item.symbol]]"></iron-icon>
+
+        <!-- <div><br/># [[index]]</div><div>icon: <span>[[item.symbol]]</span></div><div>icon: <span>[[item.symbol]]</span></div> -->
+
+      </template>
+    `;
+  }
+
+  constructor() {
+    super();
+    this.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle() {
+    this.pressed=!this.pressed;
+  }
+
+}
+customElements.define('rating-list', RatingList);
