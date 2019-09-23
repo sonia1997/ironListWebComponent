2e5897326b371f03e1cd2e73f539495664bd924c
sonia1997 <30790990+sonia1997@users.noreply.github.com>
2019-09-23 21:47:30 +0530

    Modified

--- demo-element.js
+++ demo-element.js
@@ -1,12 +1,12 @@
-// import { PolymerElement, html } from '@polymer/polymer';
-import { PolymerElement, html } from "https://unpkg.com/@polymer/polymer@^3.0.0-pre.12/polymer-element.js?module";
+import { PolymerElement, html } from '@polymer/polymer';
+// import { PolymerElement, html } from "https://unpkg.com/@polymer/polymer@^3.0.0-pre.12/polymer-element.js?module";
 import '@polymer/iron-list/iron-list.js';
 import '@polymer/polymer/lib/elements/dom-repeat.js';
 import '@polymer/iron-icon/iron-icon.js';
-// import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icons/iron-icons.js';
 
-import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js?module'
-import '@polymer/iron-list/iron-list.js';
+// import 'https://unpkg.com/tonysoft@1.53.21/iron-icons.js?module'
+// import '@polymer/iron-list/iron-list.js';
 import '../rating-list.js'
 
 class DemoElement extends PolymerElement {
